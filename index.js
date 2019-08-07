87f8c21b94d99886cb008150b1efc51750c8dc9e
Samantha Kane <s.kane133@gmail.com>
2019-08-07 03:36:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function updateDriverWithKeyAndValue(driver, key, value){
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  return  driver.key = value
+    return  driver.key = value
 
 }
