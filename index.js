94e36be720835184f84f6a16683b8d26186c34ce
Samantha Kane <s.kane133@gmail.com>
2019-08-07 03:33:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value){
-  const newDriver = {..driver}
+  const newDriver = {...driver}
 
     driver.key = value
     return driver
